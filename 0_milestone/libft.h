93b2999ff7253837faeb71452d2bd440cd67003f
Ricardo Garcia <costagarcia.r95@gmail.com>
2024-10-20 16:33:02 +0100

    libft.h

---
+++ 0_milestone/libft.h
@@ -0,0 +1,43 @@
+#ifndef LIBFT.H
+# define LIBFT.H
+
+# include <string.h>
+# include <>
+# include <>
+// Functions prototypes
+void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
+ft_memmove
+ft_memchr
+ft_memcmp
+ft_strlen
+ft_strlcpy
+ft_strlcat
+ft_strchr
+ft_strrchr
+ft_strnstr
+ft_strncmp
+ft_atoi
+ft_isalpha
+ft_isdigit
+ft_isalnum
+ft_isascii
+ft_isprint
+ft_toupper
+ft_tolower
+ft_calloc
+ft_strdup
+ft_substr
+ft_strjoin
+ft_strtrim
+ft_split
+ft_itoa
+ft_strmapi
+ft_putchar_fd
+ft_putstr_fd
+ft_putendl_fd
+ft_putnbr_fd
+
+#endif

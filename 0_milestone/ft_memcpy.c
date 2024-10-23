599034a893399622b34ee04641e12e68507a3750
Ricardo Jorge Jesus Da Costa Garcia <rjesus-d@c2r3s1.42porto.com>
2024-10-23 11:51:42 +0100

    Edited ft_memcpy

--- 0_milestone/ft_memcpy.c
+++ 0_milestone/ft_memcpy.c
@@ -1,32 +1,40 @@
 #include <stdio.h>
 #include <string.h>
 
-void    *ft_memcpy(void *dst, const void *src, size_t n)
+void    *ft_memcpy(void *dest, const void *src, size_t n)
 {
-    const unsigned char    *ptr_src;
-    unsigned char    *ptr_dst;
-    size_t i;
+        const unsigned char     *ptr_src;
+        unsigned char           *ptr_dest;
+        size_t                          i;
 
-    ptr_src = (const unsigned char *)src;
-    ptr_dst = (unsigned char *)dst;
-    i = 0;
-    while (i < n)
-    {
-        ptr_dst[i] = ptr_src[i];
-        i++;
-    }
-    return (dst);
+        ptr_src = (const unsigned char *)src;
+        ptr_dest = (unsigned char *)dest;
+        i = 0;
+        while (i < n)
+        {
+                ptr_dest[i] = ptr_src[i];
+                i++;
+        }
+        return (dest);
 }
 
-int main(void)
+int   main(void)
 {
-    char    buffer_src_1[] = "Hello";
-    char    buffer_dst_1[10] = "..........";
+        char    buffer_src_1[] = "Hello";
+        char    buffer_dest_1[] = "----------";
+        char    buffer_src_2[] = "Hello";
+        char    buffer_dest_2[] = "----------";
 
-    printf("Source 1: \"%s\"\n", buffer_src_1);
-    printf("Destination 1 before: \"%s\"\n\n", buffer_dst_1);
+        printf("Source 1:\n\"%s\"\n", buffer_src_1);
+        printf("Destination 1 before:\n\"%s\"\n", buffer_dest_1);
 
-    ft_memcpy(buffer_dst_1, buffer_src_1, 5);
-    printf("Destination 1: \"%s\"\n", buffer_dst_1);
-    return (0);
+        ft_memcpy(buffer_dest_1, buffer_src_1, 5);
+        printf("Destination 1 after (ft_memcpy()):\n\"%s\"\n\n", buffer_dest_1);
+
+        printf("Source 2:\n\"%s\"\n", buffer_src_2);
+        printf("Destination 2 before:\n\"%s\"\n", buffer_dest_2);
+        
+        memcpy(buffer_dest_2, buffer_src_2, 5);
+        printf("Destination 2 after (memcpy()):\n\"%s\"\n", buffer_dest_2);
+        return (0);
 }

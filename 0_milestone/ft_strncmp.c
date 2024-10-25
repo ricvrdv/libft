5bcee5d25fa52b524848875545c7d0311f81e5a1
Ricardo Jorge Jesus Da Costa Garcia <rjesus-d@c2r2s9.42porto.com>
2024-10-25 14:22:46 +0100

    Edited ft_strncmp

--- 0_milestone/ft_strncmp.c
+++ 0_milestone/ft_strncmp.c
@@ -9,13 +9,14 @@ int     ft_strncmp(const char *s1, const char *s2, size_t n)
         if (n == 0)
                 return (0);
         i = 0;
-        while (s1[i] != '\0' && s2[i] != '\0' && i < n - 1)
+        while (i < n)
         {
-                if ((unsigned char)s1[i] != (unsigned char)s2[i])
+                if (((unsigned char)s1[i] != (unsigned char)s2[i])
+                                || (unsigned char)s1[i] == '\0')
                         return ((unsigned char)s1[i] - (unsigned char)s2[i]);
                 i++;
         }
-        return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+        return (0);
 }
 
 int   main(int argc, char *argv[])

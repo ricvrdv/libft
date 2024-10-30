282df67c0a1e4bcb9c2cec2355f6c87cd2981948
Ricardo Jorge Jesus Da Costa Garcia <rjesus-d@c2r1s11.42porto.com>
2024-10-30 13:43:10 +0000

    Edited ft_split

--- 0_milestone/PART_2/ft_split.c
+++ 0_milestone/PART_2/ft_split.c
@@ -1,118 +1,125 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static int count_words(const char *s, char c);
-static char *extract_word(const char *s, char c);
-static void free_all(char **result, int count);
+static int      count_words(const char *s, char c);
+static char     **fill_array(const char *s, char **str_array, char c);
+static char     *extract_word(const char *s, char c);
+static void     free_all(char **result, int count);
 
 char    **ft_split(const char *s, char c)
 {
-    char **str_array;
-    int words;
-    int i;
+        char    **str_array;
+        int             word_count;
 
-    if (!s)
-        return (NULL);
-    words = count_words(s, c);
-    str_array = (char **)malloc(sizeof(char *) * (words + 1));
-    if (!str_array)
-        return (NULL);
-    i = 0;
-    while (*s != '\0')
-    {
-        while (*s == c)
-            s++;
-        if (*s != '\0')
-        {
-            str_array[i] = extract_word(s, c);
-            if (!str_array[i])
-            {
-                free_all(str_array, i);
+        if (!s)
                 return (NULL);
-            }
-            i++;
-            while (*s && *s != c)
-                s++;
-        }
-    }
-    str_array[i] = NULL;
-    return (str_array);
+        word_count = count_words(s, c);
+        str_array = (char **)malloc(sizeof(char *) * (word_count + 1));
+        if (!str_array)
+                return (NULL);
+        return (fill_array(s, str_array, c));
 }
 
-static int count_words(const char *s, char c)
+static int      count_words(const char *s, char c)
 {
-    int count;
-    int in_word;
-    int i;
+        int     count;
+        int     in_word;
+        int     i;
 
-    count = 0;
-    in_word = 0,
-    i = 0;
-    while (s[i] != '\0')
-    {
-        if (s[i] != c && in_word == 0)
+        count = 0;
+        in_word = 0;
+        i = 0;
+        while (s[i] != '\0')
         {
-            in_word = 1;
-            count++;
+                if (s[i] != c && in_word == 0)
+                {
+                        in_word = 1;
+                        count++;
+                }
+                else if (s[i] == c)
+                        in_word = 0;
+                i++;
         }
-        else if (s[i] == c)
-            in_word = 0;
-        i++;
-    }
-    return (count);
+        return (count);
 }
 
-static char *extract_word(const char *s, char c)
+static char     **fill_array(const char *s, char **str_array, char c)
 {
-    const char *end;
-    char *word;
-    int len;
-    int i;
+        int     i;
 
-    end = s;
-    while (*end && *end != c)
-        end++;
-    len = end - s;
-    word = (char *)malloc(sizeof(char) * (len + 1));
-    if (!word)
-        return (NULL);
-    i = 0;
-    while (i < len)
-    {
-        word[i] = s[i];
-        i++;
-    }
-    word[len] = '\0';
-    return (word);
+        i = 0;
+        while (*s != '\0')
+        {
+                while (*s == c)
+                        s++;
+                if (*s != '\0')
+                {
+                        str_array[i] = extract_word(s, c);
+                        if (!str_array[i])
+                        {
+                                free_all(str_array, i);
+                                return (NULL);
+                        }
+                        i++;
+                        while (*s && *s != c)
+                                s++;
+                }
+        }
+        str_array[i] = NULL;
+        return (str_array);
 }
 
-static void free_all(char **result, int count)
+static char     *extract_word(const char *s, char c)
 {
-    int i;
+        const char      *end;
+        char            *word;
+        int                     len;
+        int                     i;
 
-    i = 0;
-    while (i < count)
-    {
-        free(result[i]);
-        i++;
-    }
-    free(result);
+        end = s;
+        while (*end && *end != c)
+                end++;
+        len = end - s;
+        word = (char *)malloc(sizeof(char) * (len + 1));
+        if (!word)
+                return (NULL);
+        i = 0;
+        while (i < len)
+        {
+                word[i] = s[i];
+                i++;
+        }
+        word[len] = '\0';
+        return (word);
 }
 
-int main(void)
+static void     free_all(char **result, int count)
 {
-    char    *string = "Oops I did it again!";
-    char    delimiter = ' ';
-    char    **str_array;
+        int     i;
 
-    printf("Original string: \"%s\"\n", string);
-    str_array = ft_split(string, delimiter);
-    printf("Split strings:\n{\n");
-    while (*str_array != 0)
-    {
-        printf("\"%s\"\n", *str_array);
-        str_array++;
-    }
-    printf("}\n");
-    return (0);
+        i = 0;
+        while (i < count)
+        {
+                free(result[i]);
+                i++;
+        }
+        free(result);
+}
+
+int     main(void)
+{
+        char    *string = "Oops I dit it again!";
+        char    delimiter = ' ';
+        char    **str_array;
+
+        printf("Original string: \"%s\"\n", string);
+        str_array = ft_split(string, delimiter);
+        printf("Split strings:\n{\n");
+        while (*str_array != 0)
+        {
+                printf("\"%s\"\n", *str_array);
+                str_array++;
+        }
+        printf("}\n");
+        return (0);
 }

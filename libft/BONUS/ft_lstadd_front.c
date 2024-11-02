c76a7fd01c7c43ff6c553af82e1c64aba93f61f8
Ricardo Garcia <costagarcia.r95@gmail.com>
2024-11-02 19:52:57 +0000

    Added BONUS functions

--- libft/BONUS/ft_lstadd_front.c
+++ libft/BONUS/ft_lstadd_front.c
@@ -1,7 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+t_list  *ft_lstnew(void *content);
+void    ft_lstadd_front(t_list **lst, t_list *new);
+
+typedef struct  s_list
+{
+    void            *content;
+    struct s_list   *next;
+}                   t_list;
+
 void    ft_lstadd_front(t_list **lst, t_list *new)
 {
     if (lst == NULL || new == NULL)
         return ;
     new->next = *lst;
     *lst = new;
+}
+
+t_list  *ft_lstnew(void *content)
+{
+    t_list  *new_node;
+    new_node = malloc(sizeof(t_list));
+    if (!new_node)
+        return (NULL);
+    new_node->content = content;
+    new_node->next = NULL;
+    return (new_node);
+}
+
+int main(void)
+{
+    t_list  *node;
+    t_list  *current;
+    t_list  *next;
+    char *content_1 = "added first";
+    char *content_2 = "added second";
+    char *content_3 = "added third";
+    char *content_4 = "added fourth";
+
+    node = ft_lstnew(&content_1);
+    node->next = ft_lstnew(&content_2);
+    ft_lstadd_front(&node, ft_lstnew(&content_3));
+    ft_lstadd_front(&node, ft_lstnew(&content_4));
+    while (node != NULL)
+    {
+        printf("Node content: %s\n", *(char **)node->content);
+        node = node->next;
+    }
+    printf("End of list.\n");
+    current = node;
+    while (current != NULL)
+    {
+        next = current->next;
+        free(current->content);
+        free(current);
+        current = next;
+    }
+    node = NULL;
+    return (0);
 }
--- libft/BONUS/ft_lstnew.c
+++ libft/BONUS/ft_lstnew.c
@@ -1,4 +1,11 @@
 #include <stdlib.h>
+#include <stdio.h>
+
+typedef struct  s_list
+{
+    void            *content;
+    struct s_list   *next;
+}                   t_list;
 
 t_list  *ft_lstnew(void *content)
 {
@@ -9,4 +16,32 @@ t_list  *ft_lstnew(void *content)
     new_node->content = content;
     new_node->next = NULL;
     return (new_node);
+}
+
+int main(void)
+{
+    t_list  *head = NULL;
+    t_list  *current;
+    t_list  *next;
+    int content1 = 65;
+    char    content2 = 'B';
+
+    head = ft_lstnew(&content1);
+    head->next = ft_lstnew(&content2);
+    while (head != NULL)
+    {
+        printf("Node content: %c\n", *(int *)head->content);
+        head = head->next;
+    }
+    printf("End of list.\n");
+    current = head;
+    while (current != NULL)
+    {
+        next = current->next;
+        free(current->content);
+        free(current);
+        current = next;
+    }
+    head = NULL;
+    return (0);
 }

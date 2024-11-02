31bea66beeab67be11b29ef2f7ef61ced35b33e0
Ricardo Garcia <costagarcia.r95@gmail.com>
2024-11-02 23:34:37 +0000

    First 3 BONUS functions

--- libft/BONUS/ft_lstadd_front.c
+++ libft/BONUS/ft_lstadd_front.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-t_list  *ft_lstnew(void *content);
-void    ft_lstadd_front(t_list **lst, t_list *new);
-
 typedef struct  s_list
 {
     void            *content;
     struct s_list   *next;
 }                   t_list;
+
+t_list  *ft_lstnew(void *content);
+void    ft_lstadd_front(t_list **lst, t_list *new);
 
 void    ft_lstadd_front(t_list **lst, t_list *new)
 {

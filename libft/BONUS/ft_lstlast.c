e28855bee67656439a25130c836407b39cbe60dc
Ricardo Garcia <costagarcia.r95@gmail.com>
2024-11-03 00:14:49 +0000

    Added ft_lstlast.c

--- libft/BONUS/ft_lstlast.c
+++ libft/BONUS/ft_lstlast.c
@@ -1,29 +1,27 @@
-#include <stdlib.h>
-#include <stdio.h>
+ #include <stdio.h>
+ #include <stdlib.h>
 
-typedef struct  s_list
+ typedef struct  s_list
 {
     void            *content;
     struct s_list   *next;
 }                   t_list;
+ 
+ t_list  *ft_lstnew(void *content);
+ t_list *ft_lstlast(t_list *lst);
 
-t_list  *ft_lstnew(void *content);
-int ft_lstsize(t_list *lst);
-
-int ft_lstsize(t_list *lst)
-{
-    int count;
-    
-    count = 0;
-    while (lst != NULL)
+  t_list *ft_lstlast(t_list *lst)
+ {
+    if (lst == NULL)
+        return (NULL);
+    while (lst->next != NULL)
     {
         lst = lst->next;
-        count++;
     }
-    return (count);
-}
+    return (lst);
+ }
 
-t_list  *ft_lstnew(void *content)
+ t_list  *ft_lstnew(void *content)
 {
     t_list  *new_node;
     new_node = malloc(sizeof(t_list));
@@ -34,30 +32,26 @@ t_list  *ft_lstnew(void *content)
     return (new_node);
 }
 
-int main(void)
-{
-    t_list  *head = NULL;
-    t_list  *current = NULL;
-    int size;
+ int    main(void)
+ {
+    t_list  *head;
+    t_list  *current;
     int content_1 = 0;
     int content_2 = 1;
     int content_3 = 2;
-    int content_4 = 3;
 
     head = ft_lstnew(&content_1);
     current = head;
     current->next = ft_lstnew(&content_2);
     current = current->next;
     current->next = ft_lstnew(&content_3);
-    current = current->next;
-    current->next = ft_lstnew(&content_4);
-    size = ft_lstsize(head);
-    printf("Size = %d", size);
+    current = ft_lstlast(head);
+    printf("Last node content: %d\n", *(int *)current->content);
     while (head != NULL)
     {
         current = head;
         head = head->next;
         free(current);
     }
-    return (0);  
-}
+    return (0);
+ }

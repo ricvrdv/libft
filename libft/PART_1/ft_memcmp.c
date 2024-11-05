485b173105b3626735ae31d4a8bfc580c9ad4d5d
Ricardo Jorge Jesus Da Costa Garcia <rjesus-d@c2r2s10.42porto.com>
2024-11-05 12:40:39 +0000

    Edited ft_memcmp

--- libft/PART_1/ft_memcmp.c
+++ libft/PART_1/ft_memcmp.c
@@ -1,16 +1,32 @@
-#include "../libft.h"
+#include <stdio.h>
+#include <string.h>
 
-int
-	ft_memcmp(const void *s1, const void *s2, size_t n)
+int     ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t			i;
+        const unsigned char     *ptr_s1;
+        const unsigned char     *ptr_s2;
+        size_t                          i;
 
-	i = 0;
-	while (i < n)
-	{
-		if (*(unsigned char*)(s1 + i) != *(unsigned char*)(s2 + i))
-			return (*(unsigned char*)(s1 + i) - *(unsigned char*)(s2 + i));
-		i++;
-	}
-	return (0);
+        ptr_s1 = (const unsigned char *)s1;
+        ptr_s2 = (const unsigned char *)s2;
+        i = 0;
+        while (i < n)
+        {
+                if (ptr_s1[i] != ptr_s2[i])
+                        return (ptr_s1[i] - ptr_s2[i]);
+                i++;
+        }
+        return (0);
 }
+
+/*int   main(void)
+{
+        int     buffer_1[] = {1, 2, 3, 4};
+        int     buffer_2[] = {1, 2, 0, 4};
+        int     result_1 = ft_memcmp(buffer_1, buffer_2, sizeof(int) * 3);
+        int     result_2 = memcmp(buffer_1, buffer_2, sizeof(int) * 3);
+
+        printf("Result (using ft_memcmp) = %d\n", result_1);
+        printf("Result (using memcmp)    = %d\n", result_2);
+        return (0);
+}*/

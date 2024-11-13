2a233933fe28731f6128899072c783e309f68b9d
Ricardo Garcia <costagarcia.r95@gmail.com>
2024-11-13 00:33:56 +0000

    Final version

--- ft_printf_ONGOING/ft_putchar_len.c
+++
@@ -1,5 +0,0 @@
-void    ft_putchar_len(char c, int *len)
-{
-        write(1, &c, 1);
-        (*len)++;
-}
--- ft_printf_ONGOING/ft_putstr_len.c
+++
@@ -1,7 +0,0 @@
-#include "libft.h"
-
-void    ft_putstr_len(char *s, int *len)
-{
-        ft_putstr_fd(s, 1);
-        len += ft_strlen(s);
-}
